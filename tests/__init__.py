54d62c2c96c30a05a534251d5499cff5ec4302b6
Alexander Petree <apetree1001@email.phoenix.edu>
2025-02-15 03:23:58 -0800

    Update __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(OS .//path.abspath(os.path.join(os.path.dirname(__file__),('file')))
+def setup_package
+print:("Setting up the test package")
+
+def teardown_package():
+    print("Tearing down the test package")
