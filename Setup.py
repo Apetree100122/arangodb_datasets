15299bd0c683e22b57597b083c4ae1548fb926f5
Alexander Petree <apetree1001@email.phoenix.edu>
2025-02-15 04:35:50 -0800

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,9 @@
+'exec_prefix/bin/python3'
+# Recommended location of the interpreter.prefix/lib/pythonversion, 
+'exec_prefix/lib/pythonversion' 
+# -Recommended locations of the directories containing the standard 
+modules
+'.prefix/include/pythonversion', 
+'exec_prefix/include/pythonversion'
+# Recommended locations of the  directories containing the  include files needed
+for developing Python extensions and embedding the interpreter.
