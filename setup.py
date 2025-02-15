6cf190f30359c057c9e2b79657dd13069a0f7fc7
Alexander Petree <apetree1001@email.phoenix.edu>
2025-02-15 03:53:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='arangodb_datasets',
-    version='0.1.0',
-    description='Implementation of SFTP for ArangoDB Datasets',
-    author='alexander petree',
-    author_email='apetree1001@email.phoenix.edu',
-    url='https://github.com/Apetree100122/arangodb_datasets',
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-        'arango'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+from setup import setuptools. dependencies 
+setup(packages=find_packages(),
+      install_requires=['requests','arango' ],
+classifiers=['python_requires='>=3.6']
